4adce697659981a56c920730c57873f2cc8a4b5c
Sanne <sannelindholm@hotmail.com>
2020-11-26 11:54:36 +0100

    Removing commented out code

--- src/components/DisplayProductData.jsx
+++ src/components/DisplayProductData.jsx
@@ -15,17 +15,6 @@ class DisplayProductData extends Component {
 		this.setState({ productData: result.data.products })
 	}
 
-	// async addToOrder(event) {
-	// 	let productID = parseInt(event.target.dataset.product)
-	// 	let headers = JSON.parse(localStorage.getItem('credentials'))
-	// 	let response = await axios.post(
-	// 		'http://localhost:3000/orders',
-	// 		{ product_id: productID },
-	// 		{ headers: header }
-	// 		)
-		
-	// }
-
 	render() {
 		let dataIndex
 		if (Array.isArray(this.state.productData) && this.state.productData.length) {
